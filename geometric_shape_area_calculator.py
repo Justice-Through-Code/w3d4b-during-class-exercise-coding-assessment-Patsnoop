6be9fc0fdf86f4d76fbadbf02977febfc9fc6672
Patsnoop <patsnoop@gmail.com>
2023-09-14 19:35:35 -0400

    -m submitting my assignment

--- geometric_shape_area_calculator.py
+++ geometric_shape_area_calculator.py
@@ -6,21 +6,23 @@ def main():
     circle_pi = math.pi # DO NOT MODIFY, this line of code is assigning the variable 'circle_pi' equal to Pi ~3.14
 
     # TODO: In terminal, print Welcome to the geometric shape area calculator!
-    
+    print("Welcome to the geometric shape are calculator!")
     # User Options
-    # Circle = 1
-    # Rectangle = 2
-    # Triangle = 3
+    Circle = 1
+    Rectangle = 2
+    Triangle = 3
     
     # TODO: Using one print statement, use string concatenation to print the options only 
     # as a single string (make sure to add a space between each option)
+    print("Circle = 1" + " " + "Rectangle = 2" + " " + "Triangle = 3")
 
     # TODO: In terminal, ask the user "Select a shape by entering 1, 2, or 3' and assign the input to a new variable named 'choice'.
-
+    choice = input("Select a shape by entering 1, 2, or 3 ", )
     # TODO: Convert the variable 'choice' to an integer data type.
-
+    choice = int(choice)
     # TODO: With one line of code, verify the variable 'choice' is indeed the data type integer, use conditional logic and print the output.  If converted correctly, the output in Terminal should read 'True'.
-  
+    print(type(choice) == int)
+
     if choice == 1:  #DO NOT REMOVE THE 'IF'
         # Calculate the area of a circle
 
@@ -29,6 +31,10 @@ def main():
         # TODO: Assign a new variable named 'area' and implement the logic to calculate the area of a circle.
         # HINT 1 : The formula to find area of a circle: 'circle_pi' times radius squared.  
         # Hint 2 : circle_pi is a variable that has been assigned on Line 9 and equals Pi in math.  
+        radius = input("Please enter the radius of the circle: ", )
+        radius = float(radius)
+        area = (radius * radius * circle_pi)
+        print(area)
 
     elif choice == 2: # DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a rectangle
@@ -36,6 +42,12 @@ def main():
         # TODO: Convert both 'length' and 'width' to float.
         # TODO: Assign a new variable 'area' and implement the logic to calculate the area of a rectangle.
         # HINT: The formula to find the area of a rectangle: length times width
+        length = input("Please enter the length of the rectangle: ", )
+        width = input("Please enter the width of the rectangle: ", )
+        length = float(length)
+        width = float(width)
+        area = (length * width)
+        print(area)
 
     elif choice == 3: #DO NOT REMOVE THE 'ELIF'
         # Calculate the area of a triangle
@@ -43,15 +55,22 @@ def main():
         # TODO: Convert both 'base' and 'height' to float.
         # TODO: Assign a new variable 'area' and implement the logic to calculate the area of a triangle.
         # HINT: The formula to find the area of a Triangle: half times base times height
+        base = input("Please enter the base of the triangle: ", )
+        height = input("Please enter the height of the triangle: ", )
+        base = float(base)
+        height = float(height)
+        area = (0.5 * base * height)
+        print(area)
 
     else:
         # TODO: If the user enters anything other than 1, 2 or 3, print statement "Invalid choice ."
+        print("Invalid choice .")
     
     if choice in [1, 2, 3]: # DO NOT MODIFY
         print(f"The area is: {area:.2f} square units.") # DO NOT MODIFY
 
     # TODO: Print a statement explaining each step required to find and complete your technical assignments.  Be specific. 
-
+    print("In order to submit technical assignments you must first ensure the problem is correct using python3 -m unittest and then use the following commands: git add, git commit, and git push. Once the file is confirmed on github, you must go back to canvas and submit the link to your github on the assignment in canvas.")
 
 if __name__ == "__main__": # DO NOT MODIFY
     main() # DO NOT MODIFY
